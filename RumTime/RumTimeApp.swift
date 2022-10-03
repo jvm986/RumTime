d6985b6d03bd72c234f99bf355d04b2cf505c272
James Maguire <jvm986@gmail.com>
2022-10-03 12:29:15 +0200

    Initial Commit

---
+++ RumTime/RumTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RumTimeApp.swift
+//  RumTime
+//
+//  Created by James Maguire on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RumTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
